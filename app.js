6f74ad24d44b8a9a8733bbbc84b2f0a66909a18a
husseinmusawii <67364284+husseinmusawii@users.noreply.github.com>
2022-04-25 04:34:20 +1000

    Update app.js

--- app.js
+++ app.js
@@ -6,18 +6,3 @@ function converter(){
         output.value +=input[i].charCodeAt(0).toString(2) + " ";
     }
 }
-
-
-// for (i=0; i < input.length; i++) {
-//      	output.value +=input[i].charCodeAt(0).toString(2) + " ";
-//     }
-
-
-// function convert() {
-//   var  output=document.getElementById("ti2");  
-//   var input=document.getElementById("ti1").value;
-//     output.value = "";
-//     for (i=0; i < input.length; i++) {
-//      	output.value +=input[i].charCodeAt(0).toString(2) + " ";
-//     }
-// }
